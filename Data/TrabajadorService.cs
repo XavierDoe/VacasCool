8dfd4fe3d5f3c8aa655ebbfa9aaa51789e553336
Xavier117 <82782851+XavierDoe@users.noreply.github.com>
2023-11-19 18:56:02 -0500

    Se adiciona formulario para actualizar

--- Data/TrabajadorService.cs
+++ Data/TrabajadorService.cs
@@ -4,6 +4,8 @@
     {
         private readonly HttpClient _httpClient;
 
+        private Trabajador _trabajador;
+
         public TrabajadorService(HttpClient httpClient)
         {
             _httpClient = httpClient;
@@ -11,6 +13,16 @@
         public async Task<List<Trabajador>> ObtenerElementos(string url)
         {
             return await _httpClient.GetFromJsonAsync<List<Trabajador>>(url);
+        }
+
+        public void SetTrabajador (Trabajador trabajador)
+        {
+            _trabajador = trabajador;
+        }
+
+        public Trabajador getTrabajador()
+        {
+            return _trabajador;
         }
     }
 }
