a2ff26cffb2fa5864cf4ff10211ef0dda06fbd49
AndersonMal <anderfusion@gmail.com>
2023-11-21 10:53:12 -0500

    Agg Vista Vacas

--- Data/VacaService.cs
+++ Data/VacaService.cs
@@ -1,12 +1,10 @@
-﻿using MongoDB.Bson;
-using Newtonsoft.Json;
-using System.Net;
-
-namespace VacasCool.Data
+﻿namespace VacasCool.Data
 {
     public class VacaService
     {
         private readonly HttpClient _httpClient;
+
+        private Vaca _vaca;
 
         public VacaService(HttpClient httpClient)
         {
@@ -15,6 +13,31 @@ namespace VacasCool.Data
         public async Task<List<Vaca>> ObtenerElementos(string url)
         {
             return await _httpClient.GetFromJsonAsync<List<Vaca>>(url);
+        }
+
+        public async Task<int> GuardarDatos(Vaca vaca)
+        {
+            // Realizar una solicitud POST a la API para guardar los datos
+            string url = "https://vacas20231113212142.azurewebsites.net/Inventario";
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync(url, vaca);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return 200;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public void SetVaca(Vaca vaca)
+        {
+            _vaca  = vaca;
+        }
+        public Vaca GetVaca()
+        {
+            return _vaca;
         }
     }
 }
