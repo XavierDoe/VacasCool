277c65f4c57da6b9bf0c580639f16132111dba19
Xavier117 <82782851+XavierDoe@users.noreply.github.com>
2023-11-21 17:40:44 -0500

    VENTA

--- Data/InventarioService.cs
+++ Data/InventarioService.cs
@@ -47,6 +47,23 @@
             }
         }
 
+        public async Task<int> EliminarDatos(string id)
+        {
+            // Realizar una solicitud POST a la API para guardar los datos
+            string url = $"https://vacas20231113212142.azurewebsites.net/Inventario/{id}";
+            HttpResponseMessage response = await _httpClient.DeleteAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return 200;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+
         public void SetInventario(Inventario inventario)
         {
             _inventario = inventario;
